1ec53d47c25b5b644639fdaf6e5b1c84abbc0bf0
Tinkering Idiot <admin@tinkeringidiot.com>
2024-02-06 00:19:33 -0500

    basic index parsing

---
+++ Parser/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using AODb.Common;
+using AODb.Data;
+
+namespace Parser
+{
+    internal class RDBParser
+    {
+        public static RdbController rdbController;
+
+        static void Main(string[] args)
+        {
+            string aoPath = args[0];
+            Directory.SetCurrentDirectory(aoPath);
+            rdbController = new RdbController(aoPath);
+            Console.WriteLine(aoPath);
+        }
+    }
+}
+
